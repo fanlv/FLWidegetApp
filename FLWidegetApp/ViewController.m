08c2bfa56ec8b1c71f75c04b3a008cc4e2191bf2
fanlv <fanlvlgh@gmail.com>
2014-12-12 16:45:57 +0800

    加入4G开关
    加入4G开关

--- FLWidegetApp/ViewController.m
+++ FLWidegetApp/ViewController.m
@@ -10,39 +10,85 @@
 #import <NotificationCenter/NotificationCenter.h>
 #import <objc/runtime.h>
 #import <CoreLocation/CoreLocation.h>
-@interface CLLocationManager (iOS8)
-+ (BOOL)locationServicesEnabled;
-//+ (void)setLocationServicesEnabled:(BOOL)newValue;
 
-+ (void)setLocationServicesEnabled:(bool)arg1;
-@end
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *showLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *mySwitch;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+
 }
+
+
 - (IBAction)showWidget:(UISwitch *)sender
 {
-    NSLog(@"%d",sender.on);
-    [objc_getClass("CLLocationManager") setLocationServicesEnabled:sender.on];
-
     
-    
-//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"prefs:root=LOCATION_SERVICES"]];
-//
 //    [[NCWidgetController widgetController] setHasContent:sender.on
 //                           forWidgetWithBundleIdentifier:@"com.cocoabit.Widget.Shortcuts"];
 }
 
+
+
+
+
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- FanLvWidget/TodayViewController.m
+++ FanLvWidget/TodayViewController.m
@@ -12,37 +12,61 @@
 #import <notify.h>
 #import <CoreLocation/CoreLocation.h>
 
-//@interface CLLocationManager (iOS8)
-//+ (BOOL)locationServicesEnabled;
-//+ (void)setLocationServicesEnabled:(BOOL)newValue;
-//@end
 
 
+//----------------------------定位相关----------------------------
+@interface CLLocationManager (iOS8)
++ (BOOL)locationServicesEnabled;
++ (void)setLocationServicesEnabled:(BOOL)newValue;
+@end
+
+
+@interface LocationServicesListController
++ (BOOL)isLocationRestricted;
++ (void)setCoreRoutineAuthorized:(BOOL)arg1;
+@end
+
+//----------------------------------------------------------------
+
+
+//----------------------------DATA 开关API------------------------
 extern BOOL CTCellularDataPlanGetIsEnabled();   // 查询
 extern void CTCellularDataPlanSetIsEnabled(BOOL enabled);   // 设置
+//----------------------------------------------------------------
 
 
-//@interface CLLocationManager (iOS8)
-//+ (BOOL)locationServicesEnabled;
-//+ (void)setLocationServicesEnabled:(BOOL)newValue;
-//@end
+//----------------------------3g/4G 开关API------------------------
+extern CFStringRef const kCTRegistrationDataStatusChangedNotification;
+extern CFStringRef const kCTRegistrationDataRateUnknown;
+extern CFStringRef const kCTRegistrationDataRate2G;
+extern CFStringRef const kCTRegistrationDataRate3G;
+extern CFStringRef const kCTRegistrationDataRate4G;
+CFArrayRef CTRegistrationCopySupportedDataRates();
+CFStringRef CTRegistrationGetCurrentMaxAllowedDataRate();
+void CTRegistrationSetMaxAllowedDataRate(CFStringRef dataRate);
+//----------------------------------------------------------------
 
 
 @interface TodayViewController () <NCWidgetProviding>
 
 @property (weak, nonatomic) IBOutlet UIButton *cellularBtn;
 @property (weak, nonatomic) IBOutlet UIButton *locationBtn;
+@property (weak, nonatomic) IBOutlet UIButton *_4GDataBtn;
 
 @end
 
 @implementation TodayViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     self.preferredContentSize = CGSizeMake(0, 60);
-    self.cellularBtn.selected = CTCellularDataPlanGetIsEnabled();
+    self.cellularBtn.selected = [self getCellDataState];
     self.locationBtn.selected = [CLLocationManager locationServicesEnabled];
+    self._4GDataBtn.selected = [self is4GOR3GStateOn];
+    
 }
+
 
 
 #pragma mark - Action
@@ -50,31 +74,28 @@ extern void CTCellularDataPlanSetIsEnabled(BOOL enabled);   // 设置
 - (IBAction)dataSwitchClick:(UIButton *)sender
 {
     sender.selected = !sender.selected;
-    CTCellularDataPlanSetIsEnabled(sender.selected);
+    [self setCellDataState:sender.selected];
+}
+
+
+- (IBAction)FourGDataBtnClick:(UIButton *)sender
+{
+    sender.selected = !sender.selected;
+    [self set4GState:sender.selected];
 }
 
 - (IBAction)locationSwitchClick:(UIButton *)sender
 {
-//    sender.selected = !sender.selected;
-//
-//    id CLLocationManager1 = objc_getClass("CLLocationManager");
-//    if([CLLocationManager1 respondsToSelector:@selector(setLocationServicesEnabled:)])
-//    {
-//        [CLLocationManager1 performSelector:@selector(setLocationServicesEnabled) withObject:NO]; //setLocationServicesEnabled:sender.selected];
-//    }
+    sender.selected = !sender.selected;
+//    [objc_getClass("CLLocationManager") setLocationServicesEnabled:sender.selected];
 //    BOOL b= [CLLocationManager locationServicesEnabled];
-//     NSLog(@"%d",b);
-
+//    NSLog(@"%d",b);
 }
 
+#pragma mark - NCWidgetProviding Delegate
 
-- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
-    // Perform any setup necessary in order to update the view.
-    
-    // If an error is encountered, use NCUpdateResultFailed
-    // If there's no update required, use NCUpdateResultNoData
-    // If there's an update, use NCUpdateResultNewData
-
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler
+{
     completionHandler(NCUpdateResultNewData);
 }
 
@@ -84,4 +105,131 @@ extern void CTCellularDataPlanSetIsEnabled(BOOL enabled);   // 设置
     return margins;
 }
 
+#pragma mark - Data 开关
+
+- (BOOL)getCellDataState
+{
+    return CTCellularDataPlanGetIsEnabled();
+}
+
+
+- (void)setCellDataState:(BOOL)enable
+{
+    CTCellularDataPlanSetIsEnabled(enable);
+}
+
+
+#pragma mark - 4G Or 3G 开关
+
+///检查4G/3G开关是否打开（不支持4G的手机是看3G是否打开）
+- (BOOL)is4GOR3GStateOn
+{
+    BOOL retVal = NO;
+    CFStringRef currentDataRates = CTRegistrationGetCurrentMaxAllowedDataRate();
+    if([self supportLte])
+    {
+        if(currentDataRates == kCTRegistrationDataRate4G)
+            retVal = YES;
+    }
+    else
+    {
+        if (currentDataRates == kCTRegistrationDataRate3G)
+            retVal = YES;
+    }
+    return retVal;
+    
+}
+
+
+-(void)set4GState:(BOOL)enable
+{
+    if (!CTCellularDataPlanGetIsEnabled())
+    {
+        return;
+    }
+    
+    BOOL currentStatus = [self is4GOR3GStateOn];
+    if (currentStatus && !enable)
+    {
+        if (![self supportLte])//不支持4G的手机用2G
+        {
+            CTRegistrationSetMaxAllowedDataRate(kCTRegistrationDataRate2G);
+        }
+        else//支持4G关掉手机以后只能用3G
+        {
+            CTRegistrationSetMaxAllowedDataRate(kCTRegistrationDataRate3G);
+        }
+        return;
+    }
+    if (!currentStatus && enable)
+    {
+        if ([self supportLte])
+        {
+            CTRegistrationSetMaxAllowedDataRate(kCTRegistrationDataRate4G);
+            return;
+        }
+        else {
+            CTRegistrationSetMaxAllowedDataRate(kCTRegistrationDataRate3G);
+            return;
+        }
+    }
+}
+
+
+
+- (BOOL)supportLte
+{
+    CFArrayRef supportedDataRates = CTRegistrationCopySupportedDataRates();
+    if (supportedDataRates) {
+        if ([(__bridge NSArray *)supportedDataRates containsObject:(__bridge id)kCTRegistrationDataRate3G])
+        {
+            if ([(__bridge NSArray *)supportedDataRates containsObject:(__bridge id)kCTRegistrationDataRate4G])
+            {
+                return YES;
+            }
+        }
+    }
+    return NO;
+}
+
+
+
+
+
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
